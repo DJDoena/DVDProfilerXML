f95ec27a108c966a494b61af21eef339bfd41fd1
Karsten Schramm <SchramKa@basfad.basf.net>
2021-02-11 19:10:51 +0100

    re-add localities de-serializer

---
+++ DVDProfilerXML/4.0.0/Localities400.NotGenerated.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using DoenaSoft.DVDProfiler.DVDProfilerHelper;
+
+// 
+// xsd.exe /c /l:cs /f /n:DoenaSoft.DVDProfiler.DVDProfilerXML.Version400 Localities400.xsd
+//
+
+namespace DoenaSoft.DVDProfiler.DVDProfilerXML.Version400
+{
+    partial class Localities
+    {
+        public static Localities Deserialize()
+        {
+            var fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "DVD Profiler", "localities.xod");
+
+            if (File.Exists(fileName))
+            {
+                var localities = DVDProfilerSerializer<Localities>.Deserialize(fileName);
+
+                return localities;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
